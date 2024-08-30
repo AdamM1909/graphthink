c08ec58e25bd1fa6b09fd148e62390e2ff79e135
Adam Myers <adam.myers1909@gmail.com>
2024-08-31 00:07:20 +0100

    reorder db

--- backend/db_interface.py
+++ backend/db_interface.py
@@ -14,31 +14,6 @@ class GraphDB:
         self.conn = sqlite3.connect(db_name, check_same_thread=False)
         self.conn.row_factory = sqlite3.Row
         self._create_tables()
-
-    def add_node(self, question, answer, x, y, node_types=[]):
-        cursor = self.conn.cursor()
-        cursor.execute("INSERT INTO nodes (question, answer, x, y) VALUES (?, ?, ?, ?)", (question, answer, x, y))
-        node_id = cursor.lastrowid
-        for node_type in node_types: cursor.execute("INSERT INTO node_type_associations (node_id, type_id) VALUES (?, ?)", (node_id, self._get_id('node_types', node_type)))
-        self.conn.commit()
-        return node_id
-    
-    def add_edge(self, source_id, target_id, edge_types=[]):
-        cursor = self.conn.cursor()
-        cursor.execute("SELECT COUNT(*) FROM edges WHERE source_id = ? AND target_id = ?", (source_id, target_id))
-        assert not cursor.fetchone()[0], "Edge already exists."
-        cursor.execute("INSERT INTO edges (source_id, target_id) VALUES (?, ?)", (source_id, target_id))
-        edge_id = cursor.lastrowid
-        for edge_type in edge_types: cursor.execute("INSERT INTO edge_type_associations (edge_id, type_id) VALUES (?, ?)", (edge_id, self._get_id('edge_types', edge_type)))
-        self.conn.commit()
-        return edge_id
-    
-    def delete_part(self, p, id):
-        cursor = self.conn.cursor()
-        self.conn.execute(f"DELETE FROM {p}_type_associations WHERE node_id = ?", (id,))
-        self.conn.execute(f"DELETE FROM {p}s WHERE id = ?", (id,))
-        self.conn.execute(f"""DELETE FROM {p}_types WHERE id NOT IN (SELECT type_id FROM {p}_type_associations)""")
-        if p == 'node': [self.delete_part(p='edge', id=eid) for eid in self.conn.execute("SELECT id FROM edges WHERE source_id = ? OR target_id = ?", (id, id)).fetchall()]
            
     def get_subgraph(self, node_types=[], edge_types=[], node_logic="AND", edge_logic="AND"):
         assert edge_logic in ['AND', 'OR']
@@ -64,11 +39,36 @@ class GraphDB:
         results = cursor.fetchall()
         nodes, edges = list(set(r['node_id'] for r in results)), list(set(r['edge_id'] for r in results))
         return nodes, edges if len(nodes) > 1 else []
+    
+    def add_node(self, question, answer, x, y, node_types=[]):
+        cursor = self.conn.cursor()
+        cursor.execute("INSERT INTO nodes (question, answer, x, y) VALUES (?, ?, ?, ?)", (question, answer, x, y))
+        node_id = cursor.lastrowid
+        for node_type in node_types: cursor.execute("INSERT INTO node_type_associations (node_id, type_id) VALUES (?, ?)", (node_id, self._get_id('node_types', node_type)))
+        self.conn.commit()
+        return node_id
+    
+    def add_edge(self, source_id, target_id, edge_types=[]):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT COUNT(*) FROM edges WHERE source_id = ? AND target_id = ?", (source_id, target_id))
+        assert not cursor.fetchone()[0], "Edge already exists."
+        cursor.execute("INSERT INTO edges (source_id, target_id) VALUES (?, ?)", (source_id, target_id))
+        edge_id = cursor.lastrowid
+        for edge_type in edge_types: cursor.execute("INSERT INTO edge_type_associations (edge_id, type_id) VALUES (?, ?)", (edge_id, self._get_id('edge_types', edge_type)))
+        self.conn.commit()
+        return edge_id
+    
+    def delete_part(self, p, id):
+        cursor = self.conn.cursor()
+        self.conn.execute(f"DELETE FROM {p}_type_associations WHERE node_id = ?", (id,))
+        self.conn.execute(f"DELETE FROM {p}s WHERE id = ?", (id,))
+        self.conn.execute(f"""DELETE FROM {p}_types WHERE id NOT IN (SELECT type_id FROM {p}_type_associations)""")
+        if p == 'node': [self.delete_part(p='edge', id=eid) for eid in self.conn.execute("SELECT id FROM edges WHERE source_id = ? OR target_id = ?", (id, id)).fetchall()]
       
     def get_types(self, part, id):
         assert part in ['node', 'edge']
         cursor = self.conn.cursor()
-        cursor.execute(f"SELECT nt.name FROM {part}_type_associations nta JOIN node_types nt ON nta.type_id = nt.id WHERE nta.id = ?", (id,))
+        cursor.execute(f"SELECT pta.name FROM {part}_type_associations pta JOIN node_types pt ON pta.type_id = pt.id WHERE pta.id = ?", (id,))
         return [row[0] for row in cursor.fetchall()]
          
     def close(self): self.conn.close()
