b7b1b0a201cd39647f76408fbcdf0c842bb96f0f
Adam Myers <adam.myers1909@gmail.com>
2024-09-05 20:53:42 +0100

    Remove old junk

--- gui/setting_gui.py
+++
@@ -1,8 +0,0 @@
-class GraphMenuSidebar(QTextEdit):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setReadOnly(True)
-    
-    def display_settings(self):
-        self.clear()
-        self.append("<h3>Settings</h3>")
