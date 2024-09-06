febca417eda3a6e7a6bf90e7f1d120ce37f910ee
Adam Myers <adam.myers1909@gmail.com>
2024-09-06 22:50:46 +0100

    QUick functions to query by id -> will change for query function later

---
+++ backend/db.py
@@ -0,0 +1,45 @@
+#   docker run \
+#   --name neo4j \
+#   -p 7474:7474 -p 7687:7687 \
+#   -v ~/neo4j/data:/data \
+#   -v ~/neo4j/logs:/logs \
+#   -e NEO4J_AUTH=neo4j/password \
+#   -e NEO4J_PLUGINS='["graph-data-science"]' \
+#   -d neo4j:latest
+# http://localhost:7474/browser/
+# graph deck
+
+from neo4j import GraphDatabase
+
+__all__ = ['GraphDB']
+
+class GraphDB:
+    def __init__(self, uri="bolt://localhost:7687", username="neo4j", password="password"):
+        self.driver = GraphDatabase.driver(uri, auth=(username, password))
+        
+    def add_node(self, question, answer, node_tags) -> int: 
+        with self.driver.session() as session:
+            return self.driver.execute_query("""CREATE (n:Node {question: $question, answer: $answer, node_tags: $node_tags}) RETURN id(n) as node_id""", 
+                                               dict(question=question, answer=answer, node_tags=node_tags),
+                                               result_transformer_=lambda r: r.single(strict=True))["node_id"]
+
+    def add_edge(self, from_id, to_id, edge_tags) -> int:
+         with self.driver.session() as session:
+             return self.driver.execute_query("""MATCH (a) WHERE id(a) = $from_id MATCH (b) WHERE id(b) = $to_id CREATE (a)-[r:RELATED_TO {edge_tags: $edge_tags}]->(b)""", 
+                                               dict(from_id=from_id, to_id=to_id, edge_tags=edge_tags))
+
+    def get_all_card_ids(self):
+        with self.driver.session() as session:
+            result = session.run("MATCH (n:Node) RETURN id(n) as id ORDER BY id(n)")
+            return [record['id'] for record in result]
+    
+    def get_card_by_id(self, id: int):
+        with self.driver.session() as session:
+            result = session.run("MATCH (n) WHERE id(n) = $id RETURN n.question as question, n.answer as answer", id=id)
+            return result.single()
+        
+    def __enter__(self): return self
+    def __exit__(self, *args): self.close() 
+    def close(self): 
+        if self.driver is not None: self.driver.close()
+               
