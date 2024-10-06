efb1c2ab9b0b05ffb46d585ebf9540db83db2d98
Adam Myers <adam.myers1909@gmail.com>
2024-10-06 15:43:36 +0100

    Replace input box after adding

--- frontend/dropdown.py
+++ frontend/dropdown.py
@@ -9,27 +9,27 @@ def get_tags():
 
 @rt('/')
 def get(): 
-    checkbox_form = Form(
-                    *[CheckboxX(id=tag, label=tag) for tag in get_tags()],
-                    Button("Submit", id='submit_btn', hx_post="/submit", hx_target="#result", hx_include='#checkbox_form'),
-                    id='checkbox_form'
-                     )
- 
+    checkbox_form = Form(*[CheckboxX(id=tag, label=tag) for tag in get_tags()], id='checkbox_form')
+    submit_btn = Button("Submit", id='submit_btn', hx_post="/submit", hx_target="#result", hx_include='#checkbox_form')
     new_tag_input = Input(id="new_tag", placeholder="Add new relationship type")
-    add_new_tag_button = Button("Add New", hx_post="/add_tag", hx_target="#submit_btn", hx_swap='beforebegin', hx_include='#new_tag')
-    
-    
-    return Div(checkbox_form, Div(new_tag_input, add_new_tag_button), Div(id="result"))
-    
-    
-@app.post('/submit')
-def submit(checkbox_form: dict):
-    checkbox_form.pop('submit_btn')
-    return ', '.join((k for k, v in checkbox_form.items() if isinstance(v, list)))
-    
+    add_new_tag_btn = Button("Add New", hx_post="/add_tag", hx_target="#checkbox_form", hx_swap='beforeend', hx_include='#new_tag')
+    return Div(checkbox_form, Div(new_tag_input, add_new_tag_btn, submit_btn), Div(id="result"))
 
 @rt('/add_tag')
-def add_tag(tag: dict): return CheckboxX(id=tag["new_tag"], label=tag["new_tag"], checked=True)
- 
+def add_tag(tag: dict):
+    new_checkbox = CheckboxX(id=tag["new_tag"], label=tag["new_tag"], checked=True)
+    refreshed_new_tag_input = Input(id="new_tag", placeholder="Add new relationship type", hx_swap_oob="true")
+    return new_checkbox, refreshed_new_tag_input
+
+@app.post('/submit')
+def submit(checkbox_form: dict): return ', '.join((k for k, v in checkbox_form.items() if isinstance(v, list)))
+
 
 serve()
+
+
+# @rt('/add_option')
+# def post(new_option: str):
+#     new_checkbox = CheckboxX(id=f"option-{new_option}", label=new_option, name="options", value=new_option, checked=True)
+#     reset_input = Input(id="new-option", name="new_option", placeholder="or add new relationship type", value="", hx_swap_oob="true")
+#     return new_checkbox, reset_input
